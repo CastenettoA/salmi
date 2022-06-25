c6e24eb3aabd5dfe0ac5cc011507256d61fd39d6
castenettoa <castenetto.andrea@gmail.com>
2022-06-25 15:21:54 +0200

    emnu 1.0

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,37 @@
   <div id="app" v-cloak>
     <nav class="navbar">
       <div class="navbar-content">
-        <!-- <div class="menu"></div> -->
+
+        <div class="sideNav" id="sideNav">
+          <div class="sideNav-header">
+            <div class="navbar-logo" v-on:click="goHome()">
+              <img src="https://cdn0.iconfinder.com/data/icons/education-869/53/23-512.png">
+              <p>Salmi<span></span>Biblici</p>
+            </div>
+            <a href="javascript:void(0)" class="sideNav-closeButton" v-on:click="sideNavClose()">&times;</a>
+          </div>
+
+          <a href="/preferiti">
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              <path fill="currentColor"
+                d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+            </svg>
+            <span>Preferiti</span></a>
+          <a href="/parole-evocatrici">
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              <path fill="currentColor"
+                d="M12,6A6,6 0 0,1 18,12C18,14.22 16.79,16.16 15,17.2V19A1,1 0 0,1 14,20H10A1,1 0 0,1 9,19V17.2C7.21,16.16 6,14.22 6,12A6,6 0 0,1 12,6M14,21V22A1,1 0 0,1 13,23H11A1,1 0 0,1 10,22V21H14M20,11H23V13H20V11M1,11H4V13H1V11M13,1V4H11V1H13M4.92,3.5L7.05,5.64L5.63,7.05L3.5,4.93L4.92,3.5M16.95,5.63L19.07,3.5L20.5,4.93L18.37,7.05L16.95,5.63Z" />
+            </svg>
+            <span>Parole Evocatrici</span></a>
+        </div>
+        <span class="sideNav-openButton" v-on:click="sideNavOpen()">
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+          </svg>
+        </span>
+
+        <div class="body-overlay" id="bodyOverlay" v-on:click="sideNavClose()"></div>
+
         <div class="navbar-logo" v-on:click="goHome()">
           <img src="https://cdn0.iconfinder.com/data/icons/education-869/53/23-512.png">
           <p>Salmi<span></span>Biblici</p>
@@ -66,6 +96,19 @@ export default {
       if (this.$route.name == 'home') return;
       this.$router.push({ name: "home" });
     },
+    sideNavOpen: function () {
+      document.getElementById('sideNav').style.width = "285px";
+      document.getElementById('bodyOverlay').style.height = "100%";
+      document.getElementById('bodyOverlay').style.opacity = "1";
+      document.body.style.overflow = 'hidden';
+    },
+    sideNavClose: function () {
+      document.getElementById('sideNav').style.width = "0";
+      document.getElementById('bodyOverlay').style.height = "0%";
+      document.getElementById('bodyOverlay').style.opacity = "0";
+      document.body.style.overflow = 'scroll';
+
+    }
   },
 };
 </script>
