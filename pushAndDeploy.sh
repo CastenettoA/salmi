9a8b1fb645968fa4058b53a6101432eae4d1cf97
castenettoa <castenetto.andrea@gmail.com>
2022-07-01 11:31:24 +0200

    automatic deploy baby

--- pushAndDeploy.sh
+++ pushAndDeploy.sh
@@ -1,12 +1,15 @@
 echo "*** automatic git push, build and deploy to ca.com ***"
-echo "*** GIT ***"
+echo "*** GIT PUSH ***"
 git add .
 git commit -m 'automatic deploy baby'
 git push
 
-echo "*** git ok, now BUILD ***"
+echo "*** BUILD DIST ***"
 # npm run build
 
-echo "*** remove /salmi, copy /salmi ***"
-rm -rf ../castenettoa.com/src/app/salmi
-cp -r ./dist/app/salmi ../castenettoa.com/src/app/
+echo "*** MOVE DIST /salmi ***"
+# rm -rf ../castenettoa.com/src/app/salmi
+# cp -r ./dist/app/salmi ../castenettoa.com/src/app/
+
+echo "*** PUSH ca.com ***"
+git -C ../castenettoa.com add .
