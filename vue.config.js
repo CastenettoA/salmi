f42b2272c28b7e2d32431eb08fa75fb7bdf99f0e
castenettoa <castenetto.andrea@gmail.com>
2022-07-01 15:01:41 +0200

    automatic deploy baby

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'dist/app/salmi/',
   publicPath: '/app/salmi/', // (1) https://medium.com/js-dojo/how-to-solve-vue-js-prod-build-assets-relative-path-problem-71f91138dd79
-  
+  pwa: {
+    manifestOptions: {
+      version: '0.1',
+    }
+  }
 })
